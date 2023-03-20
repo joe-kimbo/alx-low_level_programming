914e52e229d4bbc9d6ea13e152327803a46ca5fd
joe-akimbo <chilenjejoe@gmail.com>
2023-03-20 13:07:01 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- *this is the the initiation point
- *returns: always 0
- */
-
-int main(void)
-	{
-		puts("Programming is like building a multilingual puzzle");
-		return (0);
-	}
+	/**
+ 	*main - Entry
+ 	*Return: Always 0
+ 	*/
+ 
+ 	int main(void)
+		{
+			puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+		}
