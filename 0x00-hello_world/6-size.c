985b60eb83e2ad913e7531b957d182e60d1fe89f
joe-akimbo <chilenjejoe@gmail.com>
2023-03-20 13:59:27 -0700

    C program sizeof(char)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,4 +6,9 @@
 int main(void)
 {
 	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of int: %i byte(s)\n", sizeof(int));
+	printf("Size of long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of long long: %i byte(s)\n", sizeof(long long));
+	printf("Size of float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
