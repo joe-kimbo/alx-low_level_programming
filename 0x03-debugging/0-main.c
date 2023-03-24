f8beea0f50c1495b1e719757abf99de40872f6c3
joe-akimbo <chilenjejoe@gmail.com>
2023-03-24 07:14:53 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if interget is positive or negative
- * Res=turn: 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
