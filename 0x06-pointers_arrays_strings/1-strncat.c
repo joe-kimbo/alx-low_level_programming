fed70788456914ad1e6b4584c5e9cf3b05935782
joe-akimbo <chilenjejoe@gmail.com>
2023-03-30 05:47:25 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,6 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
+
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
@@ -25,7 +26,8 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
-	
+
 	return (dest);
 }
